b4afa655a6393c4a8dd56c950358d5760a002c0b
Donnell Harris <donnell.harris@koodomobile.com>
2022-11-15 19:15:34 -0800

    completed eqArrays function

---
+++ eqArrays.js
@@ -0,0 +1,34 @@
+function assertEqual(acutal, expected) {
+  if (acutal === expected) {
+    return console.log(`🤓 Assertion Passed: [${acutal}] === [${expected}]`);
+  } else {
+    return console.log(`🤡 Assertion Failed: [${acutal}] !== [${expected}]`);
+  }
+}
+
+function eqArrays(arr1, arr2) {
+  if (arr1.length != arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
