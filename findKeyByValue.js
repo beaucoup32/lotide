c9bed2808042543d6ab71f915924c14067c515e4
Donnell Harris <donnell.harris@koodomobile.com>
2022-11-16 23:48:33 -0800

    committing completed findVeyByVal function to project

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+function assertEqual(acutal, expected) {
+  if (acutal === expected) {
+    return console.log(`🤓 Assertion Passed: [${acutal}] === [${expected}]`);
+  } else {
+    return console.log(`🤡 Assertion Failed: [${acutal}] !== [${expected}]`);
+  }
+}
+
+function findKeyByValue(object, value) {
+  for (const key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+  return;
+}
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
