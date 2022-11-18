bd307979d1197ad8e12f2f35ea401b75f9e78893
Donnell Harris <donnell.harris@koodomobile.com>
2022-11-17 22:22:00 -0800

    implement findkey function added to project

---
+++ findKey.js
@@ -0,0 +1,25 @@
+function assertEqual(acutal, expected) {
+  if (acutal === expected) {
+    return console.log(`🤓 Assertion Passed: [${acutal}] === [${expected}]`);
+  } else {
+    return console.log(`🤡 Assertion Failed: [${acutal}] !== [${expected}]`);
+  }
+};
+
+const findKey = function(obj, callback) {
+  let output = '';
+  for (let item in obj) {
+    if(callback(obj[item])) {
+      return output += item;
+    }
+  }
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3), 'Akaleri'); // => "Alaleri"
