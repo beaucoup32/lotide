920d8e6449247f947820340c69766b8453738c9f
Donnell Harris <donnell.harris@koodomobile.com>
2022-11-21 14:32:46 -0800

    refactored eqArrays function + added test updated in test/ env

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-function assertEqual(acutal, expected) {
-  if (acutal === expected) {
-    return console.log(`🤓 Assertion Passed: [${acutal}] === [${expected}]`);
-  } else {
-    return console.log(`🤡 Assertion Failed: [${acutal}] !== [${expected}]`);
-  }
-}
-
 function eqArrays(arr1, arr2) {
   if (arr1.length != arr2.length) {
     return false;
@@ -19,16 +11,4 @@ function eqArrays(arr1, arr2) {
   }
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-
+module.exports = eqArrays;
