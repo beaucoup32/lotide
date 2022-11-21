0aeb700bcd23537af24b9a1ac13d26058bba470f
Donnell Harris <donnell.harris@koodomobile.com>
2022-11-21 14:51:16 -0800

    refactor middle function + added to test env

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-function eqArrays(arr1, arr2) {
-  if (arr1.length != arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-}
-
-function assertArraysEqual(actualArr, expectedArr) {
-  if (eqArrays(actualArr, expectedArr)) {
-    return console.log(`⚖️ Assertion Passed: [${actualArr}] === [${expectedArr}]`);
-  } else {
-    return console.log(`📉 Assertion Failed: [${actualArr}] !== [${expectedArr}]`);
-  }
-}
-
 function middle(arr) {
   if (arr.length < 3) {
     return [];
@@ -28,11 +7,4 @@ function middle(arr) {
     : [arr[arr.length / 2 - 1], arr[arr.length / 2]];
 }
 
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-
-assertArraysEqual(middle([1, 2, 3]), ([2])); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), ([3])); // => [3]
-
-assertArraysEqual(middle([1, 2, 3, 4]), ([2, 3])); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), ([3, 4])); // => [3, 4]
+module.exports = middle;
